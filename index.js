0fe4f9d9edc20f55de080f310749b208f5032c54
Kelly Rogers <kellyerogers92@gmail.com>
2023-11-10 17:35:52 -0700

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Code your solution in this file!
+const drivers = (['Antonia', 'Nuru', 'Amari', 'Mo'])
+
+function returnFirstTwoDrivers(){
+    return drivers.slice(0,2)
+};
+
+//
+
+function returnLastTwoDrivers(){
+    return drivers.slice(-2)
+};
+
+//
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+//
+
+function createFareMultiplier(x){
+   return function (fare) {
+    return fare * x
+   }
+   };
+
+   //
+
+  function fareDoubler(fare){
+    return (fare*2)
+  };
+
+  //
+
+  function fareTripler(fare){
+    return(fare*3)
+  };
+
+  //
+
+  function selectDifferentDrivers(drivers, returnFirstTwoDrivers){
+    return returnFirstTwoDrivers(drivers);
+  };
